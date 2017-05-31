f931539eb2f0b9e85e2d4b028b75200748926462
Paul Frazee <pfrazee@gmail.com>
2017-05-31 12:14:41 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var fs = require('fs')
+var parseRange = require('range-parser')
+
+module.exports = function (req, res) {
+  // get size
+  var st = fs.statSync('./video.mp4')
+
+  // handle range
+  var statusCode = 200
+  res.setHeader('Accept-Ranges', 'bytes')
+  var range = req.headers.range && parseRange(st.size, req.headers.range)
+  if (range && range.type === 'bytes') {
+    range = range[0] // only handle first range given
+    range.end = range.start + (1024*1024)
+    statusCode = 206
+    res.setHeader('Content-Range', 'bytes ' + range.start + '-' + range.end + '/' + st.size)
+    res.setHeader('Content-Length', range.end - range.start + 1)
+  }
+
+  // respond
+  res.writeHead(statusCode, 'OK', {
+    'Content-Type': 'video/mp4',
+    'Cache-Control': 'public, max-age: 60'
+  })
+  fileReadStream = fs.createReadStream('./video.mp4', range)
+  fileReadStream.pipe(res)
+
+  fileReadStream.once('error', err => {
+    console.log('Error reading file', err)
+  })
+
+  req.once('aborted', () => {
+    if (fileReadStream) {
+      fileReadStream.destroy()
+    }
+  })
+}
